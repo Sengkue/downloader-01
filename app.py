482d6f2879d014a3897db2ac9725d264e7ca87bc
sengvang.blo <skvgithub@gmail.com>
2024-10-10 20:18:59 +0700

    commit 10/10/2024 20:18:59.20

--- app.py
+++ app.py
@@ -1,41 +1,29 @@
-from flask import Flask, render_template, request, send_file, jsonify
+from flask import Flask, request, send_file, render_template
 import yt_dlp
 import os
 
 app = Flask(__name__)
-
-# Folder to save the downloaded video
-DOWNLOAD_FOLDER = os.path.join(os.getcwd(), "downloads")
-if not os.path.exists(DOWNLOAD_FOLDER):
-    os.makedirs(DOWNLOAD_FOLDER)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/download', methods=['POST'])
-def download_video():
+def download():
     url = request.form['url']
-    ydl_opts = {
-        'outtmpl': os.path.join(DOWNLOAD_FOLDER, '%(title)s.%(ext)s'),
-        'format': 'best'
-    }
-
     try:
+        ydl_opts = {
+            'format': 'best',
+            'outtmpl': 'downloads/%(title)s.%(ext)s',
+        }
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            info_dict = ydl.extract_info(url, download=True)
-            video_title = info_dict.get('title', None)
-            video_file = ydl.prepare_filename(info_dict)
-        
-        return jsonify({"status": "success", "file": video_file, "title": video_title})
-
+            info = ydl.extract_info(url, download=True)
+            filename = ydl.prepare_filename(info)
+        return send_file(filename, as_attachment=True)
     except Exception as e:
-        return jsonify({"status": "error", "message": str(e)})
-
-@app.route('/download_file/<path:filename>', methods=['GET'])
-def download_file(filename):
-    file_path = os.path.join(DOWNLOAD_FOLDER, filename)
-    return send_file(file_path, as_attachment=True)
+        return str(e), 500
 
 if __name__ == '__main__':
+    if not os.path.exists('downloads'):
+        os.makedirs('downloads')
     app.run(debug=True)
