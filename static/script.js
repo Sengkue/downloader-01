0f6edcce1b55916a6717dfb59e3558085a86c592
sengvang.blo <skvgithub@gmail.com>
2024-10-10 18:06:10 +0700

    commit 10/10/2024 18:06:10.15

--- static/script.js
+++ static/script.js
@@ -7,7 +7,7 @@ document.getElementById('downloadForm').addEventListener('submit', function(even
 });
 
 function pollProgress() {
-    setInterval(() => {
+    const intervalId = setInterval(() => {
         fetch('/progress')
             .then(response => response.json())
             .then(data => {
@@ -17,7 +17,11 @@ function pollProgress() {
                 } else if (data.status === 'finished') {
                     document.getElementById('progress-bar').style.width = '100%';
                     document.getElementById('progress-text').innerText = '100% - Download complete!';
+                    clearInterval(intervalId);  // Stop polling once finished
                 }
+            })
+            .catch(err => {
+                console.error("Error fetching progress: ", err);
             });
     }, 1000);  // Poll every second
 }
