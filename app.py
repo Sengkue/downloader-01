b553dee97923684eb620f74d128cc39dda65d423
sengvang.blo <skvgithub@gmail.com>
2024-10-10 20:29:50 +0700

    commit 10/10/2024 20:29:50.14

--- app.py
+++ app.py
@@ -14,29 +14,30 @@ def download():
     format_type = request.form['format']
     
     try:
-        # Set options based on user selection
+        # Set options for yt-dlp based on format type
         ydl_opts = {
             'format': 'bestaudio' if format_type == 'audio' else 'best',
             'outtmpl': 'downloads/%(title)s.%(ext)s',
         }
-
-        # Download the video/audio
+        
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            # Extract video/audio information and download
             info = ydl.extract_info(url, download=True)
             filename = ydl.prepare_filename(info)
 
-        # Send the file to the user
+        # Send file to the user
         response = send_file(filename, as_attachment=True)
 
-        # After sending the file, delete it from the server
-        os.remove(filename)  # This will delete the file after sending
+        # Delete the file after sending
+        os.remove(filename)
+
         return response
 
     except Exception as e:
         return str(e), 500
 
 if __name__ == '__main__':
-    # Create the downloads folder if it doesn't exist
+    # Create downloads directory if it doesn't exist
     if not os.path.exists('downloads'):
         os.makedirs('downloads')
     app.run(debug=True)
