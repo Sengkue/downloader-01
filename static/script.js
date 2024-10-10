4c8a480049b1695b109b11a6af476fe63f912566
sengvang.blo <skvgithub@gmail.com>
2024-10-10 19:14:42 +0700

    commit 10/10/2024 19:14:42.17

--- static/script.js
+++ static/script.js
@@ -2,20 +2,14 @@
 const pasteButton = document.getElementById('paste-button');
 const videoUrlInput = document.getElementById('video-url');
 
-// Check if Clipboard API is available and supported
-if (navigator.clipboard && navigator.clipboard.readText) {
-    pasteButton.addEventListener('click', function() {
-        navigator.clipboard.readText()
-            .then(text => {
-                videoUrlInput.value = text;
-            })
-            .catch(err => {
-                console.error('Failed to read clipboard contents: ', err);
-                alert('Failed to paste from clipboard. Please try again.');
-            });
-    });
-} else {
-    // If Clipboard API is not supported, disable the paste button and notify the user
-    pasteButton.disabled = true;
-    alert("Clipboard functionality is not supported in this environment. Please paste the link manually.");
-}
+pasteButton.addEventListener('click', function() {
+    // Check if the clipboard API is supported
+    navigator.clipboard.readText()
+        .then(text => {
+            videoUrlInput.value = text;
+        })
+        .catch(err => {
+            console.error('Failed to read clipboard contents: ', err);
+            alert('Failed to paste from clipboard. Please try again.');
+        });
+});
